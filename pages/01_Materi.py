b5b13537eec4feeb4c798fe8b52280d68b21b180
FirlianaMaudy <130025201+FirlianaMaudy@users.noreply.github.com>
2023-04-21 16:48:58 +0700

    Update 01_Materi.py

--- pages/01_Materi.py
+++ pages/01_Materi.py
@@ -1306,8 +1306,8 @@ analit direaksikan dengan suatu pereaksi ,kemudian hasil reaksi di titrasi (anal
 analit direaksikan dengan pereaksi berlebih. Dan kelebihan pereaksi di titrasi
             """)
         
-   if optiontitri == "Asidimetri-Alkalimetri":
-      st.markdown("<h2 style='text-align: center; color: raisin black;'>Asidimetri-Alkalimetri</h2>", unsafe_allow_html=True)
+    if optiontitri == "Asidimetri-Alkalimetri":
+        st.markdown("<h2 style='text-align: center; color: raisin black;'>Asidimetri-Alkalimetri</h2>", unsafe_allow_html=True)
         st.write(
             """
             Asidimetri adalah Bila ditentukan berapa mL larutan asam yang konsentrasinya diketahui untuk menetralkan suatu larutan basa yang kadarnya dicari.
