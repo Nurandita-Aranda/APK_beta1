50c7585cd9b99404a5b449c01bff1c36e69bdb13
Uswa09 <130147838+Uswa09@users.noreply.github.com>
2023-04-22 00:38:23 +0700

    Update 01_Materi.py

--- pages/01_Materi.py
+++ pages/01_Materi.py
@@ -1517,6 +1517,46 @@ pH + pOH = 14
         
        st.image("https://lh3.googleusercontent.com/BuEzRl2oRFpiJILdxy_neSAwZZEHF-A7p-WWqy_t6X7ORGnrJHxEIPIElrj7N_T8AFFEia5qj_d8tEvnwqdRKAyE_xrFPkD6AAAY09NbAMuaFfzzKHQZXTnhczevU3obkHRTkSyg8Lcv4S6-tMjXV0oklpjllTwZOBvw740LFxeuC70Tt2hAzFu83j10EG6vqg6K95dFQMT_4MrCIqYSI13u5-cVlOSJnmJi_w6F2giyZCXuX76l-mzw063UXzs6s0fk6qIZWZ0AFJxLvhS9Gbtw06y68zrJXId-kNvqhkDbD_kZnZ4oQw04EjUqDPxPeXa8tRlqGny2L-Y5harEB1tQBK7eA19BuvN61NOUHZI2UaMTWkpa21Zg82JYDwY6XxuXME3kukf4KRE4oTEJoTwjZuKUf2n556QMB4kzTN4cFLbPudbtn1f7S2AFFteE4-2AJiZIQQsvqeeY0emPX7eIj6kT1EPD7VLnifAtBvnCavLguqYeR7cviHWAp0JY6mWAUFWR1Wvap7RnAiysh8nQ4fMuUiJ4IMzJ1lXB8QZAZKzBi2H3GAGRJ7BxI9dxQD4TJyRYbj2fA6gcqc4Uaoxpa79Ds9eQ4S5QcRyUF9Ba4R2odabveJVCSUVYz8yzJam7gW7giabSiUNAgji8XL-7jBIcqpgNEdEdCk_XHB34YndWUyZ5lMk3Sb7VWcQwLW8Yd3OAqnpARHZuFnssxuthBoSeQ-z3Kun39sgvWKDsnOD0X0hO8WBSWbFUFtyy0LFLaS79ZNzOjSt3jWytIObH-oClDdzwSt6HPmkLd37xTIouQVePjPhBJGO9KbSt0VLdBJaTB8vMzREoNAKSxxBEV5C2oK_O86jYbAjDzU81rpcValxfl6QaecsbpnwsFsb7lp9M1SHIugJxD1fVzM2tZFw4VY3Q7rAKmA9NT13v=w901-h760-s-no?authuser=0")
        st.image("https://lh3.googleusercontent.com/4uyA5mwYNBjFgRFo59Owz7HlW-p8n6aik9QZjGtRFD9VzQCnhA8kqiLGjqvGwF4HgmWQdrukQiKgQWdV2287tAaZzr0EBJNAgEDUKLRMvyomRaiNpGtyq_vbi-MH8jOWYm20s4NkHZJcYAPasbET_LPtGm9_0NqLkoInd2QQp_AcTZp7it1aHaIsOnDbO4qBHdS3D598DUdyEN0nVLDIxjPKuUIQXOQNmwPdt7bxi_kNNLb0rvyIyKVC2HaihSNMGlN_KbSMn6vjx3IDLlUyTjZ3712KdECFxc0BAFPs2NIizaew6LGAxg0xs6q52S7LY2N8Q4M9UNSy38qKdO1fZeSeGGlgrdpnLq3nAC7r9r9EXRWqVYzVii90R44kgc3pavfOY09R9Rjvr2hfN89O5iBCqrDZv7rhXEZl0VIal20cys79cL4vC8HbNar4_E3FWpjScJIigkuzv7cc9AZ8HL_wGkq5sx-hb5rzZfRrqGm1R9q2FglORvC0BZg-08imhPTHbbj0e_8tc0CWaoQzPaZFdySFpax52EJIBoFq5RKugJ0EoDqg3uzYOimoeyu9G74JnCzWUaBtyqnrrHpBpwYaKnZtojyB-1D-YQJkusoprCYz1a5JUdDsv0k27UeQlvZcwmtO_FPL6pKzUs-naBkK42zdkA3QoMsxRp8WKEtp-7H1TBAolXhrn04fUae9sCI7sglb-TqxxHtLO9hbqF_37Gy7fnpMuNq11dPr4gokrETbA2KNAqKSjOJy9AJAtnpim6Gf_DqU2bBn8u13FPZK66sYULQ6_IxvjyF-do0ZWVQ3U0AQUFkVrg4apRL1O6Pqn_cj6wCkTdDjqqde4W9NDprj6gf7QmkVt_zPpM0cb_eAusH9DYsUQlQtMBI478zLpRwRYtuOtAdhDtB3yFDhnJsbuWiTTUkBgCdErYOK=w1124-h793-s-no?authuser=0")
+        
+        if optiontitri == "Asidimetri":
+           st.markdown("<h2 style='text-align: center; color: raisin black;'>Iodometri</h2>", unsafe_allow_html=True)
+           st.write(
+              """
+              **Bahan baku primer standarisasi asam (HCl)**
+Borax atau natrium tetraborat dekahidrat
+Na2B4O7.10 H2O ( BM =381,4 BE = BM/2)
+Na2B4O7 + 5H2O   →   2NaH2BO3 + 2H3BO3
+2NaH2BO3 + 2HCl   →  2NaCl + 2H3BO3
+Na2B4O7 + 5H2O + 2HCl   →   2NaCl + 4H3BO3
+
+Natrium karbonat ( Na2CO3) ( BM = 105,96)
+Na2CO3 + HCl   →    NaHCO3 + NaCl
+NaHCO3 + HCl   →   NaCl + H2O + CO2
+
+Standardisasi HCl 0,1 N dengan Bahan Baku Primer Boraks
+Ditimbang teliti  ± 1500 mg boraks dengan kaca arloji, larutkan ke dalam labu takar 100 mL dan tepatkan sampai tanda garis, lalu homogenkan. Larutan dipipet sebanyak 25 mL ke dalam Erlenmeyer, tambahkan 2 tetes indikator MM kemudian dititar dengan larutan HCl 0,1 N sampai larutan berubah warna menjadi merah
+""")
+       st.image("https://lh3.googleusercontent.com/F_wgX_wVZXH9a5TPRmPwLDoq_Mj7g86tqwlQK3xj0eyaSZO2nrjQKlUTIRBbqckvKN3QtNd4UyOAnCdz8_Malms4PJWO7ad9wyX6HUopRJVNKeqviw69TJll3IyZ5II1k39OReAcMGwqtEf--IDw0_GIjlWWQxEqNrJe-DkEE-K6Tvd4E-3jYCI_Ohv0heu-7p7jT1VI7mTL88ue8nzcxbhExytBVwWWb28dZkchf28POwqz4EVdtPwKuvueP71Ay7lsZaF79wKuvowZIJj7Jl_tZdWzqsu0KxgIPKFkL8VndAb9HSQ-I7FAPbwHdKPXXnxqTMsRnzlEo_c50U2Z2jTNEJzmmaP3k2ojNwQKxm-atGCqVaupbJNZphPPlTluAJbn_05jshwkkYT01Mu137_9LmGES7MJJ3U6KMgrYCCqrbKuln698y2vr76JHhXwnQkf1dIoqqUkhPX5zEXT4sJn9yx85RDv0-E3CmrVxGD0H4WAMcMMgnygf2R6yQwg2X7vclNQ04BZhnjsRlvfe6zrE_dr8K-cnCl7r5WJzKJOvxt0FIdC76XGTZ2lnVdjC5WK745sag_tKc80P0TTPzyxnaSfw0unwY5QsM4-0FGaPKd-6h2adWYomF_lbMooS6hRF6aWLvnSrYq5fkb87N7EUqgwGh0vT6Ray0c6xLFXTuKL2NZHUfNG1xGRTgjHgao07Tc3U4eXhkVXRFnvPEuv0Hb7LvA1zcxCZQBi7SHGnfEBtn153TtS73nNUL7WCt7pKKc5qDuEQ-q8H7qdgYIVMDOXWTc72-yiLb36bJSWszTZ7rdbIlkTHExB9BFp1fxbsWrIkn4Ypk5jNfynQO4jSXdF63pLY4AoBGlfY7SbPSmnD9F82i2yz11bkIcaTurhodzgZnWi0I-y5gBkqTcURPa3EQCHzkhY_JyuuEFn=w1016-h159-no?authuser=0")
+       st.write(
+           """
+           **Penetapan Kadar NaOH dan Na2CO3 dalam Campuran Secara (WARDER)**
+Na2CO3 dan NaOH dalam sampel campuran dapat ditentukan kadar
+masing-masing dengan titrasi asam-basa menggunakan larutan standar
+HCl dengan 2 indikator (pp dan SM).
+Tahap 1: Digunakan indikator pp, HCl akan bereaksi terlebih dahulu dengan natrium hidroksida, dilanjutkan dengan reaksi ion karbonat menjadi hidrogen karbonat (bikarbonat).
+Tahap 2: Ditambahkan indikator SM, HCl bereaksi dengan ion hidrogen karbonat (bikarbonat)
+
+Prosedur
+Dipipet 25 mL contoh ke dalam Erlenmeyer ditambahkan dengan sedikit air suling, dibubuhi 2-3 tetes PP, lalu dititar dengan HCl 0,1 N sehingga tercapai titik akhir hilangnya warna merah muda. Banyaknya HCl yang dipakai dicatat, misalnya a mL. Larutan dibubuhi 2-3 tetes indikator SM, titar dengan HCl sampai titik akhir dari buret tadi tanpa buret dinolkan lagi. Panaskan hingga hampir mendidih (jika warna larutan berubah warna maka dititar lagi dengan HCl). Catat mL HCl yang dipakai, misalnya b mL.
+Reaksi  
+               PP
+1). NaOH + HCl  →  NaCl + H2O                             
+                PP
+2). Na2CO3 + HCl →  NaHCO3 + NaCl
+                SM
+    NaHCO3 + HCl →  NaCl + CO2 + H2O 
+    """)
+
      
           
 
