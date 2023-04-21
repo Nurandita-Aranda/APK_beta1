6470392c3bc82670f8421c283c47c64809800912
Uswa09 <130147838+Uswa09@users.noreply.github.com>
2023-04-21 19:36:41 +0700

    Update 01_Materi.py

--- pages/01_Materi.py
+++ pages/01_Materi.py
@@ -1336,8 +1336,7 @@ Larutan stabil pada pH 9 dan 10 untuk meminimalkan
 bakteri yang dapat menguraikan S2O32- menjadi SO32- , SO42-, dan S↓
 Titik akhir : iod yang terikat dengan amilum itu pun hilang bereaksi dengan titran (S2O32-) sehingga warna biru hilang
 """)
-      st.markdown("<h2 style='text-align: center; color: raisin black;'>Kesalahan titrasi</h2>", unsafe_allow_html=True)
-      st.write(
+    st.write(
          """
 **Kesalahan titrasi**
 Kesalahan Oksigen
