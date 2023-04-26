1f8ca315fcdbf83dfa9e6ee5446824e172d71501
Nurandita-Aranda <129822180+Nurandita-Aranda@users.noreply.github.com>
2023-04-26 21:33:44 +0700

    Update 04_Intermezzo.py

--- pages/04_Intermezzo.py
+++ pages/04_Intermezzo.py
@@ -1,13 +1,10 @@
 import streamlit as st
-import webbrowser
+from PIL import Image
 
-st.markdown("<h1 style='text-align: center; color: raisin black;'>Alchemist Diary</h1>", unsafe_allow_html=True)
-st.markdown("<h2 style='text-align: center; color: raisin black;'>Mari berpetualang di kerajaan Chimeía</h2>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: raisin black;'>Intermezzo</h1>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center; color: raisin black;'>Isi waktumu dengan info-info menarik tentang unsur kimia</h2>", unsafe_allow_html=True)
 
-col1, col2, col3 = st.columns(3)
-with col1:
-    st.write(" ")
-with col2:
-    st.write("[Ayo mulai!](https://github.com/Nurandita-Aranda/Mini_Game1)")  
-with col3:
-    st.write(" ")
+
+image1 = Image.open('Resource/image/Intermezzo/tabel-periodik.jpg')
+st.image(image1,caption = "Sumber: scincenotes.org")
+
