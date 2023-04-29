85007136de4127d3df28f5b1f44bf29ee53f93f1
Nurandita-Aranda <129822180+Nurandita-Aranda@users.noreply.github.com>
2023-04-29 15:33:24 +0700

    Update 06_About_Us.py

--- pages/06_About_Us.py
+++ pages/06_About_Us.py
@@ -14,7 +14,26 @@ with col3:
   st.write(" ")  
 st.markdown("<h2 style='text-align: center; color: raisin black;'>Nurandita Aranda Putri</h2>", unsafe_allow_html=True)
 st.write("Seorang mahasiswi yang tidak bisa hidup sehari pun tanpa laptopnya")
-st.write("email    : nuranditaap6@gmail.com")
-st.write("instagram: nurandita_a.p")
+st.write("email: nuranditaap6@gmail.com")
+st.write("instagram: [nurandita_a.p](https://www.instagram.com/nurandita_a.p)")
 
 #Era
+st.markdown("<h2 style='text-align: center; color: raisin black;'>Puspita Erawati Surbakti	</h2>", unsafe_allow_html=True)
+
+#Firli
+col1, col2, col3 = st.columns(3)
+with col1:
+  st.write(" ")
+with col2:
+  image1 = Image.open('Resource/image/Profile/Nabila.jpg')
+  st.image(image1)
+with col3:
+  st.write(" ")
+st.markdown("<h2 style='text-align: center; color: raisin black;'>Nabila Firliana Maudy</h2>", unsafe_allow_html=True)
+st.write("Tiada hari tanpa musik")
+st.write("email: nabilamaudy12345@gmail.com")
+st.write("instagram: [nabila_firli1212](https://www.instagram.com/nabila_firli1212)")
+
+#Uswa
+
+#Demas
