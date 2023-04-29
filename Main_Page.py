7fc085dcbf0323ce6ffabdaa8645d3f1274f210c
Nurandita-Aranda <129822180+Nurandita-Aranda@users.noreply.github.com>
2023-04-30 00:50:13 +0700

    Update Main_Page.py

--- Main_Page.py
+++ Main_Page.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import webbrowser
 height=720
 
 st.set_page_config(
@@ -9,11 +8,22 @@ st.set_page_config(
 st.markdown("<h1 style='text-align: center; color: raisin black;'>Easy Chemistry</h1>", unsafe_allow_html=True)
 st.subheader("Tanya apa saja ke Pollo")
 
-url = 'https://wa.me/message/TULJ2JKFOI4HN1'
-if st.button("Curhat boleh?"):
-    webbrowser.open_new_tab(url)
-url1 = 'https://www.youtube.com/'
-if st.button("Kayaknya ada yang nge-bug deh, Fayans"):
-    webbrowser.open_new_tab(url1)
+Opsi1 = st.button("Curhat boleh?")
+if opsi1:
+    st.write(
+        """
+        Wah, boleh banget kak!
+        
+        kakak bisa langsung chat aja akun Fayans [>disini<](https://wa.me/message/TULJ2JKFOI4HN1). Fayans selalu siap dengerin keluh kesah kakak.
+        """)
+
+opsi2 = st.button("Kayaknya ada yang nge-bug deh, Fayans"):
+if opsi2:
+    st.write(
+        """
+        Wah, kakak bisa langsung kirim saja keluhan kakak di halaman "report" yang ada di sidebar.
+        
+        Sini kak, Fayans arahin biar nggak kesasar
+        """)
 
 st.sidebar.success("Select a page above")
