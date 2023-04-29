c51ab0e72a8a176aa5a7abaae3ccc7ebb723db4a
Nurandita-Aranda <129822180+Nurandita-Aranda@users.noreply.github.com>
2023-04-29 15:53:15 +0700

    Update 06_About_Us.py

--- pages/06_About_Us.py
+++ pages/06_About_Us.py
@@ -17,7 +17,7 @@ with col3:
   st.write(" ")  
 st.write("Seorang mahasiswi yang nggak bisa hidup sehari aja tanpa laptopnya")
 st.write("email: nuranditaap6@gmail.com")
-st.write("instagram: [nurandita_a.p](https://www.instagram.com/nurandita_a.p/)")
+st.write("instagram: [@nurandita_a.p](https://www.instagram.com/nurandita_a.p/)")
 
 #Era
 st.markdown("<h2 style='text-align: center; color: raisin black;'>Puspita Erawati Surbakti	</h2>", unsafe_allow_html=True)
@@ -29,13 +29,13 @@ col1, col2, col3 = st.columns(3)
 with col1:
   st.write(" ")
 with col2:
-  image1 = Image.open('Resource/image/Profile/Nabila.jpg')
-  st.image(image1)
+  image2 = Image.open('Resource/image/Profile/Nabila.jpg')
+  st.image(image2)
 with col3:
   st.write(" ")
 st.write("Tiada hari tanpa musik")
 st.write("email: nabilamaudy12345@gmail.com")
-st.write("instagram: [nabila_firli1212](https://www.instagram.com/nabila_firli1212/)")
+st.write("instagram: [@nabila_firli1212](https://www.instagram.com/nabila_firli1212/)")
 
 #Uswa
 st.markdown("<h2 style='text-align: center; color: raisin black;'>Uswatun Hasanah</h2>", unsafe_allow_html=True)
@@ -43,13 +43,24 @@ col1, col2, col3 = st.columns(3)
 with col1:
   st.write(" ")
 with col2:
-  image1 = Image.open('Resource/image/Profile/Uswa.png')
-  st.image(image1)
+  image4 = Image.open('Resource/image/Profile/Uswa.png')
+  st.image(image4)
 with col3:
   st.write(" ")
 st.write("Mahasiswa yang pulang setiap jumat")
 st.write("email: uswahasanah0903@gmail.com")
-st.write("instagram: [bmsn_unf](https://www.instagram.com/bmsn_unf/)")
+st.write("instagram: [@bmsn_unf](https://www.instagram.com/bmsn_unf/)")
 
 #Demas
 st.markdown("<h2 style='text-align: center; color: raisin black;'>Muhammad Demas Pandya Dharmesta</h2>", unsafe_allow_html=True)
+col1, col2, col3 = st.columns(3)
+with col1:
+  st.write(" ")
+with col2:
+  image5 = Image.open('Resource/image/Profile/Demas.jpeg')
+  st.image(image5)
+with col3:
+  st.write(" ")
+st.write("Nama saya Demas. Saya lahir tanggal 17 Juli 2004 di Bekasi. Umur saya sekarang 19 tahun. Saat ini, saya sedang menempuh pendidikan di Politeknik AKA Bogor.")
+st.write("email: demasdharmesta@gmail.com")
+st.write("instagram: [@demasdharmesta](https://www.instagram.com/demasdharmesta/)")
