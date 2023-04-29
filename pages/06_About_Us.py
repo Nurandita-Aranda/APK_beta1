7526806e5932bcb0251e680a5b4f70e80fae6cca
Nurandita-Aranda <129822180+Nurandita-Aranda@users.noreply.github.com>
2023-04-29 15:01:57 +0700

    Update 06_About_Us.py

--- pages/06_About_Us.py
+++ pages/06_About_Us.py
@@ -2,3 +2,18 @@ import streamlit as st
 
 st.markdown("<h1 style='text-align: center; color: raisin black;'>About Us</h1>", unsafe_allow_html=True)
 
+#Nurandita
+col1, col2, col3 = st.columns(3)
+with col1:
+  st.write(" ")
+with col2:
+  image1 = Image.open('Resource/image/Profile/Nurand.jpg')
+  st.image(image1)
+with col3:
+  st.write(" ")  
+st.markdown("<h2 style='text-align: center; color: raisin black;'>Nurandita Aranda Putri</h2>", unsafe_allow_html=True)
+st.write("Seorang mahasiswi yang tidak bisa hidup sehari pun tanpa laptopnya")
+st.write("email    : nuranditaap6@gmail.com")
+st.write("instagram: nurandita_a.p")
+
+#Era
