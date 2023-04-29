53b07155768204bacc46710eba129e3ac8dfb99d
Nurandita-Aranda <129822180+Nurandita-Aranda@users.noreply.github.com>
2023-04-29 15:46:27 +0700

    Update 06_About_Us.py

--- pages/06_About_Us.py
+++ pages/06_About_Us.py
@@ -15,9 +15,9 @@ with col2:
   st.image(image1)
 with col3:
   st.write(" ")  
-st.write("Seorang mahasiswi yang tidak bisa hidup sehari pun tanpa laptopnya")
+st.write("Seorang mahasiswi yang nggak bisa hidup sehari aja tanpa laptopnya")
 st.write("email: nuranditaap6@gmail.com")
-st.write("instagram: [nurandita_a.p](https://www.instagram.com/nurandita_a.p)")
+st.write("instagram: [nurandita_a.p](https://www.instagram.com/nurandita_a.p/)")
 
 #Era
 st.markdown("<h2 style='text-align: center; color: raisin black;'>Puspita Erawati Surbakti	</h2>", unsafe_allow_html=True)
@@ -35,8 +35,21 @@ with col3:
   st.write(" ")
 st.write("Tiada hari tanpa musik")
 st.write("email: nabilamaudy12345@gmail.com")
-st.write("instagram: [nabila_firli1212](https://www.instagram.com/nabila_firli1212)")
+st.write("instagram: [nabila_firli1212](https://www.instagram.com/nabila_firli1212/)")
 
 #Uswa
+st.markdown("<h2 style='text-align: center; color: raisin black;'>Uswatun Hasanah</h2>", unsafe_allow_html=True)
+col1, col2, col3 = st.columns(3)
+with col1:
+  st.write(" ")
+with col2:
+  image1 = Image.open('Resource/image/Profile/Nabila.jpg')
+  st.image(image1)
+with col3:
+  st.write(" ")
+st.write("Mahasiswa yang pulang setiap jumat")
+st.write("email: uswahasanah0903@gmail.com")
+st.write("instagram: [bmsn_unf](https://www.instagram.com/bmsn_unf/)")
 
 #Demas
+st.markdown("<h2 style='text-align: center; color: raisin black;'>Muhammad Demas Pandya Dharmesta</h2>", unsafe_allow_html=True)
