57534f86bd09a98c5d9ef48ff1e8660d63b9b5ab
Nurandita-Aranda <129822180+Nurandita-Aranda@users.noreply.github.com>
2023-05-03 19:26:52 +0700

    Update Main_Page.py

--- Main_Page.py
+++ Main_Page.py
@@ -8,29 +8,36 @@ st.set_page_config(
 st.markdown("<h1 style='text-align: center; color: raisin black;'>Easy Chemistry</h1>", unsafe_allow_html=True)
 st.subheader("Tanya apa saja ke Fayans")
 
-opsi1 = st.button("Asal mula nama Fayans dari mana?")
-opsi2 = st.button("Curhat boleh?")
-opsi3 = st.button("Kayaknya ada yang nge-bug deh, Fayans")
-opsi4 = st.button("Disini ada fitur apa aja, Fayans?")
-if opsi2:
-    st.write(
-        """
-        Wah, boleh banget kak!
-        
-        kakak bisa langsung chat aja akun Fayans [disini](https://wa.me/message/TULJ2JKFOI4HN1). Fayans selalu siap dengerin keluh kesah kakak.
-        """)
-elif opsi3:
-    st.write(
-        """
-        Wah, kakak bisa langsung kirim saja keluhan kakak di halaman "Report" yang ada di sidebar.
-        
-        Sini kak, Fayans arahin biar nggak kesasar
-        """)
+
+col1, col2 = columns(2)
+
+with col1:
+  image1 = Image.open('Resource/image/Profile/Nurandita.jpg')
+  st.image(image1)
+with col2:
+    opsi1 = st.button("Asal mula nama Fayans dari mana?")
+    opsi2 = st.button("Curhat boleh?")
+    opsi3 = st.button("Kayaknya ada yang nge-bug deh, Fayans")
+    opsi4 = st.button("Disini ada fitur apa aja, Fayans?")
+    if opsi2:
+        st.write(
+            """
+            Wah, boleh banget kak!
+            
+            kakak bisa langsung chat aja akun Fayans [disini](https://wa.me/message/TULJ2JKFOI4HN1). Fayans selalu siap dengerin keluh kesah kakak.
+            """)
+    elif opsi3:
+        st.write(
+            """
+            Wah, kakak bisa langsung kirim saja keluhan kakak di halaman "Report" yang ada di sidebar.
+            
+            Sini kak, Fayans arahin biar nggak kesasar
+            """)
     
-elif opsi4:
-    st.write(
-        """
-        Kakak punya mata, kan? Usaha dikit napa, cari! Bocah-bocah kelompok 4nya udah capek nih //nggak2 canda, nanti ini gw ketik besok
-        """)
+    elif opsi4:
+        st.write(
+            """
+            Kakak punya mata, kan? Usaha dikit napa, cari! Bocah-bocah kelompok 4nya udah capek nih //nggak2 canda, nanti ini gw ketik besok
+            """)
 
 st.sidebar.success("Select a page above")
