4f9adceaa45fc83a7726fad289e25969af271c70
Nurandita-Aranda <129822180+Nurandita-Aranda@users.noreply.github.com>
2023-05-08 17:49:28 +0700

    Update 04_Intermezzo.py

--- pages/04_Intermezzo.py
+++ pages/04_Intermezzo.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 
 st.markdown("<h1 style='text-align: center; color: raisin black;'>Intermezzo</h1>", unsafe_allow_html=True)
-st.markdown("<h2 style='text-align: center; color: raisin black;'>Isi waktumu dengan info-info menarik tentang unsur kimia</h2>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center; color: raisin black;'>Isi waktumu dengan membaca info-info menarik tentang unsur kimia</h2>", unsafe_allow_html=True)
 
 
 image1 = Image.open('Resource/image/Intermezzo/tabel-periodik.jpg')
